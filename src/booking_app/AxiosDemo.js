f15c9b93a4edeae2c42be2b2bc089a47d5ff1d1b
Shubha24 <shubhadhang@gmail.com>
2023-12-12 11:17:55 +0100

    Booking application, Axios and Router

--- src/booking_app/AxiosDemo.js
+++ src/booking_app/AxiosDemo.js
@@ -5,7 +5,9 @@ const AxiosDemo = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col'>
+                    <h2>Get all bookings</h2>
                     <button className='btn btn-info' type='button'>Get Bookings</button>
+                    
                 </div>
                 </div>
             
